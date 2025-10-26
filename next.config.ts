0afbbfc35f03b0d0722a4204d90da0660a6fe18d
khaledazam <khaled141ma@gmail.com>
2025-10-27 01:48:33 +0300

    setup project, finished base structure and done homepage

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    turbopackFileSystemCacheForDev: true
+  },
+  images:{
+    domains:["images.unsplash.com"]
+  },
   reactCompiler: true,
 };
 
---
+++ src/components/EventCard.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Link from "next/link";
+import Image from "next/image";
+interface Props {
+    title: string;
+    image:string;
+    location:string;
+    slug:string;
+    date:string;
+    time:string;
+}
+const EventCard = ({title, image, slug, time, date, location}: Props
+) => {
+    return (
+        <Link href={`/events/${slug}`} id="event-card">
+            <Image src={image} width={410} height={300} alt={title} className="poster" />
+            <div className="flex flex-row gap-2">
+                <Image src="/icons/pin.svg" alt="location" width={14} height={14} />
+                <p>{location}</p>
+            </div>
+            <p className="title">{title}</p>
+            <div>
+                <Image src="/icons/calendar.svg" alt="date" width={14} height={14} />
+                <p>{date}</p>
+            </div><div>
+                <Image src="/icons/clock.svg" alt="time" width={14} height={14} />
+                <p>{time}</p>
+            </div>
+        </Link>
+    )
+}
+export default EventCard
