52c82b40308f915bca880d261a97676941b93c15
khaledazam <khaled141ma@gmail.com>
2025-10-27 03:44:55 +0300

    install postdog, mongodb and mongoose, and make schema for booking, event and index

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,23 @@ const nextConfig: NextConfig = {
   experimental: {
     turbopackFileSystemCacheForDev: true
   },
-  images:{
-    domains:["images.unsplash.com"]
+  images: {
+    domains: ["images.unsplash.com"]
   },
   reactCompiler: true,
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+    ];
+  },
+  skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
